8bd47c13431376e27e35bb64fd5347c02f88be1b
Carlos <carlos@xtrene.com>
2021-06-20 22:14:21 +0200

    Agregar archivos de proyecto.

---
+++ Tarea03_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tarea03_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 10;
+
+            Console.WriteLine("Valor inicial de N = {0}", N);
+            Console.WriteLine("N + 77 = {0}", N += 77);
+            Console.WriteLine("N - 3 = {0}", N -= 3);
+            Console.WriteLine("N * 2 = {0}", N *= 2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
